c5cfd97b00ac294bcbc517b1ceebb7a86bec3319
ZGU <nemo.gu@gmail.com>
2025-05-29 22:19:09 +0200

    Initial clean upload of Folder Structure Copier

--- build_folder_gui.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Building Folder Structure Copier using PyInstaller...
-pyinstaller --noconsole --onefile --windowed folder_structure_gui.py --name FolderStructureCopier
-echo Build complete! Check the 'dist' folder.
-pause
